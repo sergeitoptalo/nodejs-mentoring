f3bd04568c2a7c90115befa0afa28a254c424137
Siarhei Taptala <Siarhei_Taptala@epam.com>
2018-11-30 15:28:27 +0300

    Remove extra files

--- homework-01/dist/models/Product.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Product {
-    constructor() {
-        console.log('Product Module');
-    }
-}
-exports.default = Product;
-//# sourceMappingURL=Product.js.map
--- homework-01/dist/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = __importDefault(require("../models/User"));
-const Product_1 = __importDefault(require("../models/Product"));
-exports.default = { User: User_1.default, Product: Product_1.default };
-//# sourceMappingURL=index.js.map
