5a13529d4570e589f83df84db4025035912264fc
Siarhei Taptala <Siarhei_Taptala@epam.com>
2018-12-03 11:45:02 +0300

    Move files to the root folder

--- homework-01/dist/models/User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class User {
-    constructor() {
-        console.log('User Module');
-    }
-}
-exports.default = User;
-//# sourceMappingURL=User.js.map
--- homework-01/dist/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User_1 = require("./User");
-exports.User = User_1.default;
-var Product_1 = require("./Product");
-exports.Product = Product_1.default;
-//# sourceMappingURL=index.js.map
