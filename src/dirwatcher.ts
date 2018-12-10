f6beb0ff3a358bcf0a94b69b14f9b88cbf0e7d21
Sergei Toptalo <toptalo@hotmail.com>
2018-12-10 14:39:39 +0300

    Add functionality for getting new state of watched folder

--- src/dirwatcher.ts
+++ src/dirwatcher.ts
@@ -31,6 +31,8 @@ export default class DirWatcher extends EventEmitter {
             .then((data: any) => {
                 this.state.initializing = false;
                 console.log(this.state);
+                this.getNewState()
+                .then((newState) => this.detectStateChanges(this.state, newState))
             })
             .catch((err: any) => {
                 console.log(err);
@@ -42,7 +44,7 @@ export default class DirWatcher extends EventEmitter {
         this.state.delay = delay;
         this.state.initializing = true;
 
-        return this.readDirectory().then(() => this.readFiles());
+        return this.readDirectory().then((data) => this.readFiles(data));
     }
 
     private readDirectory() {
@@ -60,9 +62,9 @@ export default class DirWatcher extends EventEmitter {
         });
     }
 
-    private readFiles() {
+    private readFiles(data: any) {
         const filePromises: any = [];
-        this.state.dirState.forEach((fileName: string) => {
+        data.forEach((fileName: string) => {
             filePromises.push(new Promise((resolve, reject) => {
                 fs.readFile(this.state.path + '/' + fileName, (err, data) => {
                     if (err) {
@@ -78,5 +80,31 @@ export default class DirWatcher extends EventEmitter {
         });
 
         return Promise.all(filePromises);
+    }
+
+    private getNewState() {
+        let newDirState: any = [];
+        let newFilesState: any = [];
+
+        return this.readDirectory()
+            .then((data: any) => {
+                newDirState = data;
+            })
+            .then((data) => {
+                this.readFiles(data)
+                    .then((data: any) => {
+                        newFilesState = data;
+                    })
+            })
+            .then(() => {
+                return {
+                    dirState: newDirState,
+                    filesState: newFilesState
+                }
+            })
+    }
+
+    private detectStateChanges(previousState: any, newState: any) {
+
     }
 }
