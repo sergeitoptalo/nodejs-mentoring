a4030823770d62a310d6a1c4f882f22ed17ecdae
Sergei Toptalo <toptalo@hotmail.com>
2018-12-10 14:40:10 +0300

    Fix lint semicolon issue

--- src/dirwatcher.ts
+++ src/dirwatcher.ts
@@ -32,7 +32,7 @@ export default class DirWatcher extends EventEmitter {
                 this.state.initializing = false;
                 console.log(this.state);
                 this.getNewState()
-                .then((newState) => this.detectStateChanges(this.state, newState))
+                .then((newState) => this.detectStateChanges(this.state, newState));
             })
             .catch((err: any) => {
                 console.log(err);
@@ -66,12 +66,12 @@ export default class DirWatcher extends EventEmitter {
         const filePromises: any = [];
         data.forEach((fileName: string) => {
             filePromises.push(new Promise((resolve, reject) => {
-                fs.readFile(this.state.path + '/' + fileName, (err, data) => {
+                fs.readFile(this.state.path + '/' + fileName, (err, fileContent) => {
                     if (err) {
                         reject(err);
                     } else {
                         if (this.state.initializing) {
-                            this.state.filesState.push(data);
+                            this.state.filesState.push(fileContent);
                         }
                         resolve(data);
                     }
@@ -90,21 +90,21 @@ export default class DirWatcher extends EventEmitter {
             .then((data: any) => {
                 newDirState = data;
             })
-            .then((data) => {
-                this.readFiles(data)
-                    .then((data: any) => {
-                        newFilesState = data;
-                    })
+            .then((directoryContent) => {
+                this.readFiles(directoryContent)
+                    .then((files: any) => {
+                        newFilesState = files;
+                    });
             })
             .then(() => {
                 return {
                     dirState: newDirState,
-                    filesState: newFilesState
-                }
-            })
+                    filesState: newFilesState,
+                };
+            });
     }
 
     private detectStateChanges(previousState: any, newState: any) {
-
+        console.log('Changes detected');
     }
 }
