efaecbe8f6bfcc75327ec2e32c29c57c69c24577
sergeitoptalo <toptalo@hotmail.com>
2018-12-16 19:24:45 +0300

    Fix Importer for removed file

--- src/Importer/Importer.ts
+++ src/Importer/Importer.ts
@@ -23,22 +23,29 @@ export default class Importer {
     public import(importConfig: IImportConfig) {
         const filePromises: Array<Promise<IComparisonResult>> = [];
         importConfig.changes.forEach((change: any) => {
-            filePromises.push(new Promise<IComparisonResult>((resolve, reject) => {
-                fs.readFile(
-                    `${importConfig.path}/${change.fileName}`,
-                    { encoding: 'utf8' },
-                    (err, fileContent) => {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve({
-                                fileContent: csvjson.toObject(fileContent)[0],
-                                fileName: change.fileName,
-                                status: change.status,
-                            });
-                        }
-                    });
-            }));
+            if (change.status !== 'removed') {
+                filePromises.push(new Promise<IComparisonResult>((resolve, reject) => {
+                    fs.readFile(
+                        `${importConfig.path}/${change.fileName}`,
+                        { encoding: 'utf8' },
+                        (err, fileContent) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve({
+                                    fileContent: csvjson.toObject(fileContent)[0],
+                                    fileName: change.fileName,
+                                    status: change.status,
+                                });
+                            }
+                        });
+                }));
+            } else {
+                filePromises.push(Promise.resolve({
+                    fileName: change.fileName,
+                    status: change.status,
+                }));
+            }
         });
 
         return Promise.all(filePromises);
