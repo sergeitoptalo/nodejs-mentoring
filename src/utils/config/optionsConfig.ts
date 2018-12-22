41e1f9db3a3cd7a3cf34713d58ebc3b2839a1246
Sergei Toptalo <toptalo@hotmail.com>
2018-12-22 10:45:48 +0300

    Homework-03: add options configuration

---
+++ src/utils/config/optionsConfig.ts
@@ -0,0 +1,14 @@
+export const optionsConfig = {
+    action: {
+        full: '--action',
+        shortcut: '-a',
+    },
+    file: {
+        full: '--file',
+        shortcut: '-f',
+    },
+    help: {
+        full: '--help',
+        shortcut: '-h',
+    },
+};
